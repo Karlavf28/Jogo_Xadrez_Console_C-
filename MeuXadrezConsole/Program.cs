be04c8f01e7866bf87c6f51bc36a7f2ab18f2dbf
Karla Azambuja <karlavf28@gmail.com>
2020-03-07 16:47:17 -0300

    Projeto Iniciado

---
+++ MeuXadrezConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuXadrezConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
